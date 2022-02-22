13299cad5d2c289c0e30b1d594ec8b1d1875ebaa
cortina17 <diego.cortinagarcia@gmail.com>
2022-02-22 09:39:22 +0100

    legacy exercise

---
+++ src/components/ExerciseEighteen.vue
@@ -0,0 +1,102 @@
+<template>
+  <div>
+    <h2>18.- Calificación Examen</h2>
+    <div>
+      <input type="text" placeholder="Nombre" id="alumName" required />
+
+      <input type="text" placeholder="Materia" id="subjectName" required />
+
+      <input
+        type="number"
+        placeholder="Escriba nota entre 0 y 10"
+        id="numExerciseEighteen"
+        required
+      />
+
+      <button v-on:click="examGrade()">Aceptar</button>
+    </div>
+    <div id="resultExerciseEighteen"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ExerciseEighteen",
+  data: () => ({}),
+
+  methods: {
+    examGrade() {
+      let name = document.getElementById("alumName").value;
+      let subject = document.getElementById("subjectName").value;
+      let num = parseInt(document.getElementById("numExerciseEighteen").value);
+
+      let printResult = document.getElementById("resultExerciseEighteen");
+      printResult.innerHTML = "<p>";
+
+      if (num) {
+        if (num < 3) {
+          printResult.innerHTML +=
+            name +
+            ", tu nota en " +
+            subject +
+            " es: " +
+            num +
+            " - La calificación es MUY DEFICIENTE";
+          return;
+        }
+        if (num < 5) {
+          printResult.innerHTML +=
+            name +
+            ", tu nota en " +
+            subject +
+            " es: " +
+            num +
+            " - La calificación es INSUFICIENTE";
+          return;
+        }
+        if (num < 6) {
+          printResult.innerHTML +=
+            name +
+            ", tu nota en " +
+            subject +
+            " es: " +
+            num +
+            " - La calificación es SUFICIENTE";
+          return;
+        }
+        if (num < 7) {
+          printResult.innerHTML +=
+            name +
+            ", tu nota en " +
+            subject +
+            " es: " +
+            num +
+            " - La calificación es BIEN";
+          return;
+        }
+        if (num < 9) {
+          printResult.innerHTML +=
+            name +
+            ", tu nota en " +
+            subject +
+            " es: " +
+            num +
+            " - La calificación es NOTABLE";
+          return;
+        }
+        if (num <= 10) {
+          printResult.innerHTML +=
+            name +
+            ", tu nota en " +
+            subject +
+            " es: " +
+            +num +
+            " - La calificación es SOBRESALIENTE";
+          return;
+        }
+      }
+      printResult.innerHTML += "Introduzca una nota entre 0 y 10";
+    },
+  },
+};
+</script>
