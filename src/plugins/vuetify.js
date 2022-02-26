31f01ba581b58b603a0b0f6b31b96ca0baf3f495
Cortina17 <diego.cortinagarcia@gmail.com>
2022-02-26 12:28:19 +0100

    solved test bug
    console logged a error message due to been using mount instead of shallowmount

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -2,6 +2,6 @@ import Vue from "vue";
 import Vuetify from "vuetify/lib/framework";
 import "vuetify/dist/vuetify.min.css";
 
-Vue.use(Vuetify);
+Vue.use(Vuetify, {});
 
 export default new Vuetify({});
--- tests/unit/tests.spec.js
+++ tests/unit/tests.spec.js
@@ -1,9 +1,9 @@
-import { mount } from "@vue/test-utils";
+import { shallowMount } from "@vue/test-utils";
 import Home from "@/views/Home.vue";
 
 describe("Notes", () => {
   it("rendering component", () => {
-    const wrapper = mount(Home);
+    const wrapper = shallowMount(Home);
     expect(wrapper.exists()).toBe(true);
   });
 });
